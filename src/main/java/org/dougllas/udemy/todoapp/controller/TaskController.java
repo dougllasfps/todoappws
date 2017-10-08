83c9cf6d4b345325d324473b8e1cd9e808ea7d95
Dougllas Sousa <dougllasfps@gmail.com>
2017-10-08 17:58:32 -0300

    Atualizando o controller de Tasks

--- src/main/java/org/dougllas/udemy/todoapp/controller/TaskController.java
+++ src/main/java/org/dougllas/udemy/todoapp/controller/TaskController.java
@@ -1,22 +1,43 @@
 package org.dougllas.udemy.todoapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.dougllas.udemy.todoapp.model.Task;
+import org.dougllas.udemy.todoapp.repository.TaskReporitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Criado por dougllas.sousa em 03/10/2017.
  */
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskController implements Serializable{
 
-    @RequestMapping("/teste")
+    @Autowired
+    private TaskReporitory reporitory;
+
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public String teste(){
-        return "hello";
+    public ResponseEntity<List<Task>> list(){
+        return new ResponseEntity<List<Task>>(reporitory.findAll(), HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Task> add(@RequestBody Task task){
+        reporitory.save(task);
+        return new ResponseEntity<Task>(task, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Integer> add(@RequestParam("id") Integer id){
+        reporitory.delete(id);
+        return new ResponseEntity<Integer>(id, HttpStatus.OK);
     }
 }
